98b77e3c1fc8a3c2d8c207c93520c63960359a42
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-25 21:49:40 +0100

    m

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-function UserProfile(){
-    return(
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+ const UserProfile = (props) => {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
+      </div>
     );
-}
- export default UserProfile;
+  };
+  export default UserProfile;
