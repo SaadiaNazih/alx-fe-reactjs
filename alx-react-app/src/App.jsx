6f526bdfa84c248899ff8141b9147df8961a12bb
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-25 19:56:25 +0100

    m

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,7 +12,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
+      <Header/>
+      <MainContent/>
+      <Footer/>
       <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
