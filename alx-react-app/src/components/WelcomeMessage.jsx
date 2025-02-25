b7c96d88ea8897c522a0e6c65b7027388ec400c5
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-25 19:51:47 +0100

    m

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,8 +1,8 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Ciao a tutti, sto imparando React ad ALX!</h1>
-            <p>Sto imparando a conoscere JSX!</p>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
