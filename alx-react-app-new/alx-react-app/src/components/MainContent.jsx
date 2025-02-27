c61f25fea62cedd6c1188088190e2056d602c3a4
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-27 19:51:00 +0100

    m

---
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainContent(){
+    return (
+        <main>
+            <h1>I love to visit New York, Paris, and Tokyo.</h1>
+        </main>
+    );
+}
+export default MainContent;
+
+
