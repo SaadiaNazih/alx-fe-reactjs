ee63b703e296d1790d199eaeb5b14892ea4daaeb
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-27 19:59:57 +0100

    m

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 function  Header(){
     return(
-        <header style={{color: 'red'}}>
-            <h1>My Favorite Cities</h1>
+        <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+             <h1>My Favorite Cities</h1>
         </header>
     );
 }
