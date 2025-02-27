c67de7e051949d812a4f69d17c27de7677f56baf
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-27 20:16:12 +0100

    m

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         <MainContent />
         <Footer />
         <WelcomeMessage />
+        <Counter/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
