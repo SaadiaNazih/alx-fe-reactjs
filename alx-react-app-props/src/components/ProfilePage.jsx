7d0bed5aff8270f496f9d6ac7c7bff02e4a7597f
Saadia Nazih <saadia.nazih92@gmail.com>
2025-02-28 18:02:28 +0100

    m

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,10 @@
+function App() {
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+    return <ProfilePage userData={userData} />;
+  }
+  function App() {
+    const [count, setCount] = useState(0)
+
+  };
+    export default ProfilePage;
