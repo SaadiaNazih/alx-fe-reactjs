00492a5cd123610ff0eee01bfa2c22adfdbdf891
Saadia Nazih <saadia.nazih92@gmail.com>
2025-03-02 11:56:09 +0100

    m

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -17,7 +17,7 @@ import { useState } from 'react';
      };
 
      return (
-       <div style={{ padding: '20px' }}>
+       <div style={{ backgroundColor: 'grey', padding: '20px' }}>
          <h1>Contact Us</h1>
          <form onSubmit={handleSubmit}>
            <input
