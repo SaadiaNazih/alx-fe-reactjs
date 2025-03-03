a5163626e7342f5a5b1aeabe68987e344d14e305
Saadia Nazih <saadia.nazih92@gmail.com>
2025-03-03 12:19:31 +0100

    m

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Sharing Application</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;
+
